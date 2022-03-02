57e5e3925725e58677038d9b2dc9943ac42b0c96
GerkinDev <alexandre.j.p.germain@gmail.com>
2022-03-02 18:15:42 +0100

    feat(pluginutils): add source map support in markdown replacer

--- packages/typedoc-pluginutils/src/markdown-replacer.ts
+++ packages/typedoc-pluginutils/src/markdown-replacer.ts
@@ -1,13 +1,31 @@
 import assert from 'assert';
 
-import { isNil, isNumber, isString, once } from 'lodash';
-import MagicString from 'magic-string';
+import { isNil, isNumber, isString, last, once } from 'lodash';
 import { Context, Converter, JSX, MarkdownEvent, SourceFile } from 'typedoc';
 
 import { ABasePlugin } from './base-plugin';
 import { CurrentPageMemo } from './current-page-memo';
 import { reflectionSourceUtils, textUtils } from './utils';
 
+interface ISourceEdit {
+	from: number;
+	to: number;
+	source: string;
+	replacement: string;
+}
+interface ISourceMapContainer {
+	editions: ISourceEdit[];
+	getEditionContext: ( position: number ) => ( {
+		line: number;
+		column: number;
+		expansions: ISourceMapContainer[];
+		index: number;
+		source: string;
+	} );
+	label: string;
+	plugin: ABasePlugin;
+	regex: RegExp;
+}
 const spitArgs = ( ...args: Parameters<Parameters<typeof String.prototype.replace>[1]> ) => {
 	const indexIdx = args.findIndex( isNumber );
 	if( isNil( indexIdx ) ){
@@ -53,11 +71,20 @@ export class MarkdownReplacer {
 			];
 		}
 	} );
-	private static readonly _eventsOffsets = new WeakMap<MarkdownEvent, MagicString>();
+	private static readonly _mapContainers = new WeakMap<MarkdownEvent, ISourceMapContainer[]>();
 
 	private readonly _logger = this.plugin.logger.makeChildLogger( 'MarkdownReplacer' );
 	private readonly _currentPageMemo = new CurrentPageMemo( this.plugin );
 
+	/**
+	 * Get the list of source map containers for the given event.
+	 *
+	 * @param event - The event to get source maps for.
+	 * @returns the source map list.
+	 */
+	private static _getEventMapContainers( event: MarkdownEvent ): ISourceMapContainer[] {
+		return this._mapContainers.get( event ) ?? [];
+	}
 
 	public constructor(
 		protected readonly plugin: ABasePlugin,
@@ -70,10 +97,11 @@ export class MarkdownReplacer {
 	 *
 	 * @param regex - The regex to match.
 	 * @param callback - The callback to execute with fullMatch, captures, & a source hint.
+	 * @param label - The replacer name.
 	 */
-	public bindReplace( regex: RegExp, callback: MarkdownReplacer.ReplaceCallback ) {
+	public bindReplace( regex: RegExp, callback: MarkdownReplacer.ReplaceCallback, label = `${this.plugin.name}: Unnamed markdown replace` ) {
 		assert( regex.flags.includes( 'g' ) );
-		this.plugin.application.renderer.on( MarkdownEvent.PARSE, this._processMarkdown.bind( this, regex, callback ), undefined, 100 );
+		this.plugin.application.renderer.on( MarkdownEvent.PARSE, this._processMarkdown.bind( this, regex, callback, label ), undefined, 100 );
 	}
 
 
@@ -82,42 +110,76 @@ export class MarkdownReplacer {
 	 *
 	 * @param regex - The regex to match.
 	 * @param callback - The callback to execute with fullMatch, captures, & a source hint.
+	 * @param label - The replacer name.
 	 * @param event - The event to modify.
 	 */
 	private _processMarkdown(
 		regex: RegExp,
 		callback: MarkdownReplacer.ReplaceCallback,
+		label: string,
 		event: MarkdownEvent,
 	) {
-		// TODO: Proper sourcemap support
-		let magic: MagicString | undefined = new MagicString( event.parsedText );
-		event.parsedText = event.parsedText.replace(
+		const mapContainers = MarkdownReplacer._getEventMapContainers( event );
+		const sourceFile = this._currentPageMemo.hasCurrent ? reflectionSourceUtils.getReflectionSourceFileName( this._currentPageMemo.currentReflection ) : undefined;
+		const relativeSource = sourceFile ? this.plugin.relativeToRoot( sourceFile ) : undefined;
+		const originalText = event.parsedText;
+		const edits: ISourceEdit[] = [];
+		const getCtxInParent = last( mapContainers )?.getEditionContext ??
+			( pos => ( { ...textUtils.getCoordinates( originalText, pos ), source: originalText, index: pos, expansions: [] } ) );
+		event.parsedText = originalText.replace(
 			regex,
 			( ...args ) => {
-				const { captures, fullMatch, index, source } = spitArgs( ...args );
+				const { captures, fullMatch, index } = spitArgs( ...args );
 				const replacement = callback(
 					{ fullMatch, captures },
 					() => {
-						const sourceFile = reflectionSourceUtils.getReflectionSourceFileName( this._currentPageMemo.currentReflection );
-						if( !sourceFile ){
+						if( !relativeSource ){
 							return 'UNKNOWN SOURCE';
 						}
-						const pos = textUtils.getCoordinates( source, index );
-						const { line, column } = pos;
+						const { line, column, expansions } = getCtxInParent( index );
 						const posStr = line && column ? `:${line}:${column}` : '';
-						return this.plugin.relativeToRoot( sourceFile ) + posStr;
+						const expansionContext = ` (in expansion of ${expansions.concat( [ thisContainer ] ).map( e => e.label ).join( ' ⇒ ' )})`;
+						return relativeSource + posStr + expansionContext;
 					} );
 				if( isNil( replacement ) ){
 					return fullMatch;
 				}
 				const replacementStr = typeof replacement === 'string' ? replacement : JSX.renderElement( replacement );
-				try {
-					magic = magic?.overwrite( index, index + fullMatch.length, replacementStr );
-				} catch( _e: any ){
-					magic = undefined;
-				}
+				edits.push( { from: index, to: index + fullMatch.length, replacement: replacementStr, source: fullMatch } );
 				return replacementStr;
 			} );
+		const thisContainer: ISourceMapContainer = {
+			regex,
+			editions: edits,
+			label,
+			plugin: this.plugin,
+			getEditionContext: ( pos: number ) => {
+				const { offsetedPos, didEdit } = edits.reduce(
+					( acc, edit ) => {
+						const isAfterEdit = edit.from <= acc.offsetedPos;
+						if( !isAfterEdit ){
+							return acc;
+						}
+						const _didEdit = edit.from + edit.replacement.length > acc.offsetedPos;
+						return {
+							offsetedPos: acc.offsetedPos + ( _didEdit ?
+								edit.from - acc.offsetedPos :
+								edit.source.length - edit.replacement.length ),
+							didEdit: _didEdit || acc.didEdit,
+						};
+					},
+					{ offsetedPos: pos, didEdit: false } );
+				const parentCtx = getCtxInParent( offsetedPos );
+				if( didEdit ){
+					parentCtx.expansions = [ ...parentCtx.expansions, thisContainer ];
+				}
+				return parentCtx;
+			},
+		};
+		MarkdownReplacer._mapContainers.set( event, [
+			...mapContainers,
+			thisContainer,
+		] );
 	}
 }
 export namespace MarkdownReplacer {
--- packages/typedoc-pluginutils/types/test.d.ts
+++ packages/typedoc-pluginutils/types/test.d.ts
@@ -1,1 +1,10 @@
 import './expect-type';
+
+declare global {
+	namespace jest {
+		interface Expect {
+			// eslint-disable-next-line @typescript-eslint/prefer-function-type -- Declaration merging
+			<T = any>( actual: T, message?: string ): JestMatchers<T>;
+		}
+	}
+}
