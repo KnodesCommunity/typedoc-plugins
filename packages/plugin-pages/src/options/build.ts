1634a2ee40b97dfff9c81f4574e9ca72c8df47fc
GerkinDev <alexandre.j.p.germain@gmail.com>
2022-03-04 13:32:53 +0100

    feat(plugin-pages): validate pages option

--- packages/plugin-pages/src/options/build.ts
+++ packages/plugin-pages/src/options/build.ts
@@ -1,10 +1,36 @@
+import assert, { AssertionError } from 'assert';
+
+import { difference, isArray, isNil, isObject, isString } from 'lodash';
 import { LogLevel, ParameterType } from 'typedoc';
 
-import { OptionGroup } from '@knodes/typedoc-pluginutils';
+import { OptionGroup, catchWrap } from '@knodes/typedoc-pluginutils';
 
 import type { PagesPlugin } from '../plugin';
-import { EInvalidPageLinkHandling, IPluginOptions } from './types';
+import { EInvalidPageLinkHandling, IPageNode, IPluginOptions } from './types';
 
+const pageKeys: Array<keyof IPageNode> = [ 'children', 'childrenDir', 'childrenOutputDir', 'childrenSourceDir', 'output', 'source', 'title' ];
+const wrapPageError = ( path: string[], index: number ) => ( err: any ) => {
+	if( err instanceof AssertionError ){
+		const pathStr = path.length > 0 ? ` in ${path.map( p => JSON.stringify( p ) ).join( ' ⇒ ' )}` : '';
+		return `Invalid page${pathStr} @ index ${index}`;
+	} else {
+		return err;
+	}
+};
+const checkPage = ( page: unknown, path: string[] ): asserts page is IPageNode => {
+	assert( page && isObject( page ), 'Page should be an object' );
+	const _page = page as Record<string, unknown>;
+	assert( 'title' in _page && isString( _page.title ), 'Page should have a title' );
+	const extraProps = difference( Object.keys( _page ), pageKeys );
+	assert( extraProps.length === 0, `Page should not have extra props ${JSON.stringify( extraProps )}` );
+	if( 'children' in _page && !isNil( _page.children ) ){
+		assert( isArray( _page.children ), 'Page children should be an array' );
+		const thisPath = [ ...path, _page.title as string ];
+		_page.children.forEach( ( c, i ) => catchWrap(
+			() => checkPage( c, thisPath ),
+			wrapPageError( thisPath, i ) ) );
+	}
+};
 export const buildOptions = ( plugin: PagesPlugin ) => OptionGroup.factory<IPluginOptions>( plugin )
 	.add( 'enablePageLinks', {
 		help: 'Whether or not @page and @pagelink tags should be parsed.',
@@ -30,6 +56,14 @@ export const buildOptions = ( plugin: PagesPlugin ) => OptionGroup.factory<IPlug
 	.add( 'pages', {
 		help: 'Actual pages definitions.',
 		type: ParameterType.Mixed,
+		validate: v => {
+			if( v ){
+				assert( isArray( v ), 'Pages should be an array' );
+				v.forEach( ( p, i ) => catchWrap(
+					() => checkPage( p, [] ),
+					wrapPageError( [], i ) ) );
+			}
+		},
 	}, v => {
 		v = v ?? [];
 		// TODO: Better checks
