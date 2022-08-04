6ff4978c93d5164acf3d2b3aa18d2fc335dab314
Alexandre Germain <alexandre.germain@craft.ai>
2022-08-04 11:36:30 +0200

    chore(pluginutils): remove dead code

--- packages/pluginutils/src/utils/reflection-source.ts
+++ packages/pluginutils/src/utils/reflection-source.ts
@@ -1,40 +1,12 @@
-import { isNumber } from 'lodash';
-import { ProjectReflection, Reflection, SourceReference, normalizePath } from 'typedoc';
+import { Reflection, SourceReference, normalizePath } from 'typedoc';
 
 import { PluginAccessor, getApplication, getPlugin } from '../base-plugin';
-import { getCoordinates } from './text';
 
 export const getReflectionSourceFileName = ( reflection?: Reflection ) => {
 	if( !reflection ){
 		return;
 	}
 	return reflection.sources?.[0].fullFileName;
-};
-export const getPageSourceCoordinates = ( reflection: Reflection | undefined, position: number ): {line: number; column: number; file: string} | undefined => {
-	const sourceRef = reflection?.sources?.[0];
-	if( !sourceRef || !reflection.comment ){
-		return undefined;
-	}
-	const commentParts = reflection instanceof ProjectReflection && reflection.readme ?
-		reflection.readme :
-		reflection.comment.summary;
-	const coordinates = getCoordinates(
-		commentParts[0].text,
-		position );
-	return {
-		...coordinates,
-		line: sourceRef.line + coordinates.line - 1,
-		file: sourceRef.fileName,
-	};
-};
-
-export const getSourceLocationBestClue = ( reflection?: Reflection, position?: number ) => {
-	const pageSourceCoordinates = isNumber( position ) ? getPageSourceCoordinates( reflection, position ) : undefined;
-	if( pageSourceCoordinates ){
-		return `${pageSourceCoordinates.file}:${pageSourceCoordinates.line}:${pageSourceCoordinates.column}`;
-	} else {
-		return getReflectionSourceFileName( reflection ) ?? 'UNKNOWN SOURCE';
-	}
 };
 
 export const createSourceReference = ( pluginAccessor: PluginAccessor, absoluteFilename: string, line?: number, character?: number ) => {
@@ -43,9 +15,7 @@ export const createSourceReference = ( pluginAccessor: PluginAccessor, absoluteF
 	const repo = ( getApplication( pluginAccessor ).converter.getComponent( 'source' ) as any )?.getRepository( source.fullFileName );
 	source.url = repo?.getURL( source.fullFileName );
 	if ( source.url && repo ) {
-		source.url += `#${repo.getLineNumberAnchor(
-			source.line,
-		)}`;
+		source.url += `#${repo.getLineNumberAnchor( source.line )}`;
 	}
 	return source;
 };
